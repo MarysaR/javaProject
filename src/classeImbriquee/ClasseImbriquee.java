71f7341f4e3b3aebca59e7eaacd3eb985e38a46b
neycha <regentmarysapro@gmail.com>
2024-12-18 11:35:33 +0100

    feat: classe imbriquée

---
+++ src/classeImbriquee/ClasseImbriquee.java
@@ -0,0 +1,17 @@
+package classeImbriquee;
+
+public class ClasseImbriquee {
+  static final double TAUX=1.2;
+
+  static class Interne {
+    public double calculTTC(double prix){
+      return prix*ClasseImbriquee.TAUX;
+    }
+  }
+
+  public static void Main(String[] args){
+    ClasseImbriquee.Interne interne = new ClasseImbriquee.Interne();
+    System.out.println(interne.calculTTC(120));
+  }
+}
+
