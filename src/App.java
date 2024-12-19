1d2c2985a6f8a4d83b3b7ad8912e3238510bf683
neycha <regentmarysapro@gmail.com>
2024-12-19 10:43:52 +0100

    feat: FileReader function

--- src/App.java
+++ src/App.java
@@ -1,10 +1,36 @@
-public class App {
- /**
-  * @param args
-  * @throws Exception
-  */
-    public static void main(String[] args) throws Exception{
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-        System.out.println("Hello, World!");;
+public class App {
+    public static void main(String[] args) {
+
+        FileReader fileReader = null;
+        BufferedReader in = null;
+        try {
+            fileReader = new FileReader("carre.txt");
+            in = new BufferedReader(fileReader);
+            String readLine;
+            while (true) {
+                readLine = in.readLine();
+                if (readLine == null) break;
+                System.out.println(readLine);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
     }
 }
